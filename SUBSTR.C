3e662010dbba27e4666a597204906cdb4a35e4d8
M Jeffin Manuel <manueljeffin@hotmail.com>
2016-07-04 18:45:35 +0530

    Update SUBSTR.C

--- SUBSTR.C
+++ SUBSTR.C
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main()
 {
+	//see file paper
 	char str[50];
 	int t,wall=0,i=1,j=0,max=0;
 	printf("Enter a string:\n");
 	scanf("%s",str);
+	//wall sets the limit till which we have to scan , t is the length 
+	//wall with recognize that something is repeating, at the end of the repeating substring and update itself,
+	//for eg: in ababc, wall will update itself wen j is at index 3 and i is at index 4
 	while(str[i]!='\0')
 	{
 		t=1;
